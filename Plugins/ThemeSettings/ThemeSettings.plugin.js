064c299ab1c27c484a65b86ccb27a1311fe5e2d8
wolfscape96 <57021963+wolfscape96@users.noreply.github.com>
2019-10-25 19:38:22 -0500

    Update ThemeSettings.plugin.js

--- Plugins/ThemeSettings/ThemeSettings.plugin.js
+++ Plugins/ThemeSettings/ThemeSettings.plugin.js
@@ -1,4 +1,4 @@
-//META{"name":"ThemeSettings","website":"https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/ThemeSettings","source":"https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/ThemeSettings/ThemeSettings.plugin.js"}*//
+//META{"name":"ThemeSettings","website":"https://www.geekgirlauthority.com/wp-content/uploads/2019/08/The-Rise-of-Skywalker-Poster-2-crop.jpg/mwittrien/BetterDiscordAddons/tree/master/Plugins/ThemeSettings","source":"https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/ThemeSettings/ThemeSettings.plugin.js"}*//
 
 class ThemeSettings {
 	getName () {return "ThemeSettings";}
